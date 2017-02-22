ea72b94753533e87dae304b6e5ff1974ab41fe2b
krishnakandula <krishna.c.kandula@gmail.com>
2017-02-22 08:03:15 -0600

    Interprocess communication now works.

--- src/cpu/CPUController.java
+++ src/cpu/CPUController.java
@@ -1,10 +1,47 @@
 package cpu;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 /**
  * Created by Krishna Chaitanya Kandula on 2/20/17.
  */
 public class CPUController {
+    private static final String CLASS_PATH = "out/production/CPU-Memory-Simulator memory.MemoryController";
     public static void main (String... args){
+        try
+        {
+            int x;
+            Runtime rt = Runtime.getRuntime();
 
+            Process proc = rt.exec("java -classpath " + CLASS_PATH);
+
+            InputStream is = proc.getInputStream();
+            OutputStream os = proc.getOutputStream();
+
+            PrintWriter pw = new PrintWriter(os);
+            pw.printf("Greg\n");
+            pw.flush();
+
+            Scanner sc = new Scanner(is);
+            String line;
+
+            while(sc.hasNext() && !(line = sc.nextLine()).isEmpty()) {
+                System.out.println(line);
+            }
+
+            proc.waitFor();
+
+            int exitVal = proc.exitValue();
+
+            System.out.println("Process exited: " + exitVal);
+
+        }
+        catch (Throwable t)
+        {
+            t.printStackTrace();
+        }
     }
 }
--- src/memory/MemoryController.java
+++ src/memory/MemoryController.java
@@ -1,20 +1,26 @@
 package memory;
 
+import java.util.Scanner;
+
 /**
  * Created by Krishna Chaitanya Kandula on 2/20/17.
  */
 public class MemoryController {
     public static void main(String... args){
-        SystemMemory memory = new SystemMemory();
-        memory.initialize();
-//        memory.printMemory();
+        System.out.println("test");
+        try
+        {
+            Scanner sc = new Scanner(System.in);
+            String line = sc.nextLine();
+
+            for (int i=0; i<line.length(); i++)
+                System.out.println(":)");
+        }
+        catch (Throwable t)
+        {
+            t.printStackTrace();
+        }
     }
 
-    private static int read(int address){
-        return 0;
-    }
 
-    private static void write(int address, int data){
-        return;
-    }
 }
